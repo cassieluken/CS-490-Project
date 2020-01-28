d0ea9aa732f2713ff34d6b6a61adbb5864e2aadb
cassieluken <40001862+cassieluken@users.noreply.github.com>
2020-01-28 14:25:28 -0800

    studentAnswersM.php middle
    curl student answers to back

---
+++ studentAnswersM.php
@@ -0,0 +1,9 @@
+<?php
+//---- IMPORTING CURL FUNCTION ---
+include "curlFunc.php";
+/*RECEIVE DATA FROM POST REQUEST*/
+$indata = file_get_contents("php://input");
+//---- CONTACTING BACK SERVER ----
+$url = "https://web.njit.edu/~ts557/studentAnswers.php";
+echo curlyHair($url, $indata);
+?>
